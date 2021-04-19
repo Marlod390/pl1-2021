c1d0a8a8b20758e5a105f209b2ac1d0d33795ab9
Marlod390 <67206431+Marlod390@users.noreply.github.com>
2021-04-19 22:03:05 +0200

    Update 01Hw.scala

--- exercises/01Hw.scala
+++ exercises/01Hw.scala
@@ -45,27 +45,38 @@ case class False() extends Exp // constant false
 case class And(lhs: Exp, rhs: Exp) extends Exp
 case class Or(lhs: Exp, rhs: Exp) extends Exp
 case class Not(e: Exp) extends Exp
+case class Impl(lhs: Exp, rhs: Exp) extends Exp
 
 /**
 Tasks:
-
        1) Implement the missing parts of the interpreter for these formulae
           (the eval function).
           Test the correctness by evaluating the example proposition given
           below and add at least two more examples and test against these.
-
        2) Add implication as a new kind of expression "Impl" and extend
           the interpreter accordingly. Add at least two examples and test.
 */
 
 def eval(e: Exp) : Boolean = e match {
-  case True()    => sys.error("not yet implemented")
-  case False()   => sys.error("not yet implemented")
-  case And(l, r) => sys.error("not yet implemented")
-  case Or(l, r)  => sys.error("not yet implemented")
-  case Not(e)    => sys.error("not yet implemented")
+  case True()    => true
+  case False()   => false
+  case And(l, r) => eval(l) && eval(r)
+  case Or(l, r)  => eval(l) || eval(r)
+  case Not(e)    => !eval(e)
+  case Impl(s,f) => !eval(s) || eval(f)
 }
 
 val exampleProposition = And(Not(True()), False()) // should evaluate to false
+val test1= Or(False(),True())// examples for test 1,should evaluate to true 
+val test2= Or(False(),Not(True()))// examples for test 2,should evaluate to false
+val impltest1 = Impl(Or(True(),False()), And(True(),False()))// examples for test implication 1,should evaluate to false
+val impltest2 = Impl(And(True(),False()), Or(True(),False()) )// examples for test implication 1,should evaluate to true
+def main(args: Array[String]) : Unit = {
+  println(eval(exampleProposition))
+  println(eval(test1))
+  println(eval(test2))
+  println(eval(impltest1))
+  println(eval(impltest2))
+}
 
 }
